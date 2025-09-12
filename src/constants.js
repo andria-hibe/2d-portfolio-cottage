92d55dda36d3387b1cd2631df872f3165f2ca7cb
Andria Hibe <andriacohibe@gmail.com>
2025-09-13 02:55:43 +1200

    chore: update links

--- src/constants.js
+++ src/constants.js
@@ -20,16 +20,16 @@ export const DIALOGUE_DATA = {
   I now bring that perspective into software development, combining technical skills with strong communication and user empathy.`,
   'sofa-table': `This is my comfy sofa, my favourite spot to relax with a book or dive into one of my hobbies.
   I am drawn to detail-oriented activities that reward focus and patience and you will often find me working on pottery, miniatures, or embroidery.`,
-  tv: `That's my TV. I love watching movies and I'm currently making my way through <a href="https://www.bfi.org.uk/sight-and-sound/greatest-films-all-time" target="_blank">The Sight and Sound Greatest Films of All Time 2022</a> list.
+  tv: `That's my TV. I love watching movies and I'm currently making my way through The Sight and Sound Greatest Films of All Time 2022 list.
   It's taking a while but I'm enjoying it!`,
   bed: `This is where I sleep. I love sleeping and I have the superhuman ability of being able to sleep anywhere and never having jet lag.`,
   resume: `This is my desk and here you can find my resume. <a href="https://github.com/andria-hibe/resume/blob/dfa375354d0b82e84383bf72485e9d3737360a37/Resume%20-%20Andria%20Hibe.pdf" target="_blank">Have a look!</a>
     Contact me at <a href="mailto:andriacohibe@gmail.com">andriacohibe@gmail.com</a> if you have any interesting job opportunities!`,
-  project: `This portfolio is made with the Kaplay.js library which is a library for making games in JavaScript. It's my first time using it and it's been a lot of fun learning about it. Check out the code on <a href="https://github.com/andria-hibe/2d-portfolio" target="_blank">Github</a>.`,
+  project: `This portfolio is made with the Kaplay.js library which is a library for making games in JavaScript. It's my first time using it and it's been a lot of fun learning about it. Check out the code on <a href="https://github.com/andria-hibe/2d-portfolio-cottage" target="_blank">Github</a>.`,
   library: `I love reading books. I'm currently reading Drive Your Plow Over the Bones of the Dead by Olga Tokarczuk and Blindness by Jose Saramago.
   I usually read two books at a time, one in a physical book and one in an e-book. I'd love to hear if you have any book recommendations.`,
   hobbies: `I love going on adventures, especially through travel and diving. I also enjoy hiking, surfing, and skiing whenever I get the chance.
   My most recent adventure was a diving liveaboard on the Great Barrier Reef, where I got to witness the migration of dwarf minke whales. On rainy days, you can also find me at home taking care of my plants as I slowly turn my home into a jungle.`,
-  refrigerator: `I love baking. If you Google my name you might just find one of my recipes for brownies that was published in a local magazine. If you work with me, chances are you'll end up with some homemade baked goods at one point or another.`,
+  refrigerator: `I love baking. If you Google my name, you might just find one of my recipes for brownies that was published in a local magazine. If you work with me, chances are you'll end up with some homemade baked goods at one point or another.`,
   exit: `If you want to exit this portfolio, just close the tab. Thanks for visiting!`,
 }
